261d280b2fb394da045e911fbdd9dadeeee2c205
Paul Kang <paul@paul-kang.com>
2021-04-21 23:12:52 +0800

    wip

--- js/app.js
+++ js/app.js
@@ -16,4 +16,25 @@ async function subscribe(){
   document.querySelector('#sub-string').style.height='250px'
   document.querySelector('#sub-string').style.display = "block";
   console.log(JSON.stringify(push));
+  
+}
+
+async function test(){
+  
+  // find the max id
+  var max_id=0;
+  var max_sub;
+  const snapshot = await db.collection('Subscriptions').get();
+  snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+    if (parseInt(doc.id) > max_id) {
+      max_id = parseInt(doc.id);
+      max_sub = doc.data()
+    }
+  });
+  
+  const snapshot = await db.collection('Subscriptions').doc('1').set(
+  {
+    'subscription': JSON.stringify(push)
+  });  
 }
