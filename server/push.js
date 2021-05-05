b41c165ad97cb34e8793e7ad81afef79fa9925e0
Paul Kang <paul@paul-kang.com>
2021-05-05 22:12:40 +0800

    wip

--- server/push.js
+++ server/push.js
@@ -29,14 +29,14 @@ async function main(){
   var topicToNotify = "MAQ";
   var subs = await readSub();
   for (var i = 0; i< subs.length; i++){
-    console.log(subs[i]);
     const sub_topics = JSON.parse(subs[i].topics);
+    //console.log(sub_topics);
     if (sub_topics.includes(topicToNotify)){      
       if (subs[i].subscription == undefined || subs[i].subscription == "") {
         console.log('subscription is undefined or empty');
       }
       
-      console.log("Send notification to ", topicToNotify);
+      console.log("Send notification to ", subs[i].client);
       let sub = subs[i].subscription;
       var payload = {
         // 自定義，可以夾帶客製資料
@@ -44,10 +44,11 @@ async function main(){
         'body':  'Greeting to ' + subs[i].user + " for topic:" + topicToNotify
       }
       
-      console.log(JSON.parse(sub));
-      console.log(payload);
-      await push.sendNotification(JSON.parse(sub), JSON.stringify(payload))
-      .catch(err => console.log('Send Notification Error:', (err.statusCode=='410')? "Subscription is not avaliable":err.statusCode));  
+//      console.log(JSON.parse(sub));
+//      console.log(payload);
+      
+      //await push.sendNotification(JSON.parse(sub), JSON.stringify(payload))
+      //.catch(err => console.log('Send Notification Error:', (err.statusCode=='410')? "Subscription is not avaliable":err.statusCode));  
 
       console.log("done");
       
@@ -68,13 +69,13 @@ async function main(){
   //MAQ Firefox to https://rizap-crm.github.io/pwa-ninja-food/
   //{"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABgkpmdZUEGvpOeRko0BQaIM-4AK9JtFnn1dXI9_vIw4SnYP9z94wu-bl8E6LMAFdW28iwdgbvYEiAuPXFyTunnLkDGRjIa4AH4tT50FpZn-7mYYPbkKUk7VcK-NQypZn06gUhwYjCB4xNepBRwfasNjR2TvsM6NwfwEt6cbtTHi1c_2D8","keys":{"auth":"WdqB7A7CMlWw1qmlm5B9hg","p256dh":"BPOmEUqANIkPD-IGjlFQgyBJweGIsleqKW9-s4gWbImrPcreqFfvlDjWg1HRK3yWDzT8lkZOaHz-BWWLde5yYgo"}};
   
-  // Vivo PC to https://rizap-crm.github.io/pwa-ninja-food/    
+  // VivoPC Chrome to https://rizap-crm.github.io/pwa-ninja-food/    
   //{"endpoint":"https://fcm.googleapis.com/fcm/send/dJ2KgJymJsU:APA91bFMIQtGwMn_7zauksu9h50LDN47e25bajBPdc9uyHv1Fx6ZPw_B7h5xy1oWG4MfyCzvNlOew8VL6tHW8XqdIydr6RmFTPQjGjb4AHSt5NWJnpLVakRQYiCcd8hqJ3WCZMz_RNla","expirationTime":null,"keys":{"p256dh":"BMmnZJWQ2_9y0oWuCygmZc3ZSv7JPjJ0FqJfCudldTSbi1BSOHr1bYiWGVveQJ7Ju8FpTu3UhCytyjSsu09N4RA","auth":"Xfg5ct0VbpFW7V6aMWW1ig"}};
 
-  // Vivo PC Android emulator to //rizap-crm.github.io/pwa-ninja-food/
+  // VivoPC Android emulator to //rizap-crm.github.io/pwa-ninja-food/
   //{"endpoint":"https://fcm.googleapis.com/fcm/send/fR6wtmbiPsQ:APA91bFOaPf-_RCc1Mlx1ckg3HNC1WFuoO6Q83FvzoYuPp9tC5P-NwLlz0OeAByBY_YWye-TCQjCRW9ItFovW2t_iBr_7qj9i_LD3wxjcbYcU3hPn1BvCobN6NSj0TQl7EQfhzm8ys3q","expirationTime":null,"keys":{"p256dh":"BMgm3-4cZahRFtdsqzEBB7nul5R8QslqDB3q07WSQ3mvuiYzsI5RM8nqAdywtOSc_tZr6N2nHj9lcnR22p4ZH5w","auth":"nl-5gok81EXpj1gctKJBAw"}};
 
-  // Mac mini Chrome to //rizap-crm.github.io/pwa-ninja-food/
+  // MacMini Chrome to //rizap-crm.github.io/pwa-ninja-food/
   //{"endpoint":"https://fcm.googleapis.com/fcm/send/f_ZK3cZf-Wk:APA91bFXVOWEIE695g12IrV1ZQIrrh3zdI8dfhS8Bw-ugQW8sfgE58LJrBfmIXr1bB7zwc_l_8XONEjP6KumKXosWHmn1zV4w4BzI-5OAoY9MQMnv9mFq0i-G8fghizl3Kr_FZzIAd-3","expirationTime":null,"keys":{"p256dh":"BG5wenCzR2YjSCSEfvmUbQg3sdY5g92WKJVcNDCr3-DxreHR9tx3vOGH-LHhckV8mikBbgxGGjY3O0WPQFkd0XE","auth":"1rRIUNs6lR5uPfE6HOcUoA"}};
 
 }
